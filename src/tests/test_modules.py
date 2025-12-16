bf83f6dd8cba436e4762a7f2b0dec00e8f5b000d
Grant Lemons <grantlemons@aol.com>
2025-12-15 17:03:45 -0700

    change autograder to work with project 4

--- src/tests/test_modules.py
+++ src/tests/test_modules.py
@@ -4,41 +4,28 @@ import subprocess
 from gradescope_utils.autograder_utils.decorators import weight, number
 
 class TestBase(unittest.TestCase): 
-    def runStudentCode(self, dirname, name):
-        res = subprocess.call(['./run_student_code.sh', dirname])
+    def assertValidAssembly(self, name):
+        res = subprocess.call(['n2tAssembler', f'/autograder/source/{name}.asm'])
         if res != 0:
-            raise AssertionError(f'Unable to run student\'s Jack Analyzer on {name}.jack!')
+            raise AssertionError(f'Unable to assemble student\'s ASM output!')
 
-    def assertDiffMatch(self, dirname, name):
-        res = subprocess.call(['diff', f'/autograder/grader/tests/expected-outputs/{dirname}/{name}.xml', f'/autograder/source/{dirname}/{name}.xml', '--strip-trailing-cr'])
+    def assertCPUMatches(self, name):
+        res = subprocess.call(['n2tCPUEmulator', f'/autograder/source/{name}.tst'])
         if res != 0:
-            diff = subprocess.check_output(['/bin/sh', '-c', f'diff /autograder/grader/tests/expected-outputs/{dirname}/{name}.xml /autograder/source/{dirname}/{name}.xml --strip-trailing-cr ; exit 0'], text=True)
-            print(f'Files differ!\n{diff}')
-            raise AssertionError(f'Student\'s XML did not match the provided XML file!')
+            raise AssertionError(f'Unable to run CPU Emulator!')
 
-    def assertCorrectAnalyzer(self, testpath):
-        dirname, name = testpath.split('/')
-        self.runStudentCode(dirname, name)
-        self.assertDiffMatch(dirname, name)
-        subprocess.run(['mv', f'/autograder/source/{dirname}/{name}.xml', '/autograder/outputs/'])
+    def assertCorrectJack(self, name):
+        self.assertValidAssembly(name)
+        self.assertCPUMatches(name)
+        subprocess.run(['mv', f'/autograder/source/{name}.out', '/autograder/outputs/'])
 
 class TestModules(TestBase): 
-    @weight(47.5/3)
+    @weight(95/2)
     @number(1)
-    def test_square_main(self):
-        self.assertCorrectAnalyzer('Square/Main')
+    def test_mult(self):
+        self.assertCorrectJack('Mult')
 
-    @weight(47.5/3)
+    @weight(95/2)
     @number(2)
-    def test_square_square(self):
-        self.assertCorrectAnalyzer('Square/Square')
-
-    @weight(47.5/3)
-    @number(3)
-    def test_square_square_game(self):
-        self.assertCorrectAnalyzer('Square/SquareGame')
-
-    @weight(47.5)
-    @number(4)
-    def test_array_test_main(self):
-        self.assertCorrectAnalyzer('ArrayTest/Main')
+    def test_fill(self):
+        self.assertCorrectJack('Fill')
